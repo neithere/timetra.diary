491017d067f8274764770722c769256b3faee36e
Andrey Mikhaylenko <neithere@gmail.com>
2012-05-25 16:32:45 +0600

    http: added two features: a) user can edit fact, b) user can search facts. Currently search is identical to filtering by activity but in perspective the latter should become much stricter.

--- timetra/web/__init__.py
+++ timetra/web/__init__.py
@@ -4,28 +4,78 @@
 Web application
 ===============
 """
-import flask
+from flask import Blueprint, Flask, redirect, render_template, request, url_for
+import wtforms as wtf
 
 from timetra import storage
 
 
-blueprint = flask.Blueprint('timetra', __name__)
+blueprint = Blueprint('timetra', __name__)
+
+
+class TagListField(wtf.Field):
+    widget = wtf.widgets.TextInput()
+
+    def _value(self):
+        return u' '.join(self.data) if self.data else u''
+
+    def process_formdata(self, valuelist):
+        self.data = []
+        if valuelist:
+            self.data = [x.strip() for x in valuelist[0].split(',')]
+
+
+class FactForm(wtf.Form):
+    category = wtf.SelectField()
+    activity = wtf.TextField()
+    description = wtf.TextAreaField()
+    tags = TagListField()
+    start_time = wtf.DateTimeField()
+    end_time = wtf.DateTimeField(u'End Time', [wtf.validators.Optional()])
 
 
 @blueprint.route('/')
 def dashboard():
-    return flask.render_template('dashboard.html', storage=storage)
+    return render_template('dashboard.html', storage=storage)
+
+
+@blueprint.route('search/')
+def search():
+    query = request.values.get('q')
+    facts = storage.get_facts_for_day(date=-1, search_terms=query)
+    return render_template('search.html', storage=storage, facts=facts)
 
 
 @blueprint.route('activities/<activity>/')
 def activity(activity):
     facts = storage.get_facts_for_day(date=-1, search_terms=activity)
-    return flask.render_template('activity.html', storage=storage,
-                                 activity=activity, facts=facts)
+    return render_template('activity.html', storage=storage,
+                           activity=activity, facts=facts)
+
+
+@blueprint.route('facts/<int:fact_id>/edit', methods=['GET', 'POST'])
+def edit_fact(fact_id):
+    fact = storage.hamster_storage.get_fact(fact_id)
+    form = FactForm(request.form, fact)
+    categories = storage.hamster_storage.get_categories()
+    form.category.choices = [(x['name'], x['name']) for x in categories]
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(fact)
+        #-- Hamster peculiarities -----------------------------------------------------------
+        # Update is performed via remove/insert, so the
+        # fact_id after update should not be used anymore. Instead use the ID
+        # from the fact dict that is returned by this function
+        #
+        new_id = storage.hamster_storage.update_fact(fact_id, fact)
+        #
+        #----------------------------------------------------------------------
+        return redirect(url_for('timetra.edit_fact', fact_id=new_id))
+
+    return render_template('edit.html', storage=storage, fact=fact, form=form)
 
 
 def create_app(config, debug=False):
-    app = flask.Flask(__name__)
+    app = Flask(__name__)
     app.debug = debug
     if config:
         app.config.from_object(config)
